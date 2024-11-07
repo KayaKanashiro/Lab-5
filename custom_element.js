dc469e61df8dee9a90582fb6051e26b38e98a28a
KayaKanashiro <uxkanashiro@gmail.com>
2024-11-07 01:52:54 +0000

    done

--- custom_element.js
+++ custom_element.js
@@ -8,3 +8,20 @@ class Centre extends HTMLElement{
 }
 
 customElements.define("x-center", Centre);
+
+class Year extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+
+customElements.define("x-year", Year);
+
+class Pink extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<div style="color:pink">
+        ${this.innerHTML}</div>`
+    }
+}
+
+customElements.define("x-pink", Pink);
